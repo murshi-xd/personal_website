ad9fd73aabb9b8eceecd9d608b3bf337a31d8700
murshid PP <muhammedmurshidpp0@gmail.com>
2022-08-28 23:41:02 +0530

    about and project div's are added

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Designer", "Coder", "Developer"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+})
+
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+    speed: 250
+});
